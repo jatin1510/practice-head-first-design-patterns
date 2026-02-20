cd77b45ff90c967a97d469e19fa674ac55ee74ba
Jatin Ranpariya <jatinranpariya1510@gmail.com>
2026-02-21 00:06:23 +0530

    Add Person class with Builder pattern and update Main to demonstrate usage

--- src/main/java/playground/Main.java
+++ src/main/java/playground/Main.java
@@ -1,15 +1,12 @@
 package playground;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FilterInputStream;
-import java.util.zip.ZipInputStream;
-
 public class Main {
     public static void main(String[] args) {
-        FileInputStream fileInputStream = null;
-        FilterInputStream filterInputStream = null;
-        BufferedInputStream bufferedInputStream = null;
-        ZipInputStream zipInputStream = null;
+        Person person = new Person.Builder()
+                .age(10)
+                .name("Vivan")
+                .build();
+
+        System.out.println(person);
     }
 }
---
+++ src/main/java/playground/Person.java
@@ -0,0 +1,49 @@
+package playground;
+
+public class Person {
+    private final String name;
+    private final int age;
+
+    private Person(Builder builder) {
+        this.name = builder.name;
+        this.age = builder.age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static class Builder {
+        private String name;
+        private int age;
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Person build() {
+            if (name == null) {
+                throw new IllegalStateException("Name is required");
+            }
+            return new Person(this);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
